4e5311cc86a0ed387ba085d93f3183b703f9709e
billel khiri <145929851+bi2lelkhr@users.noreply.github.com>
2026-01-28 19:50:26 +0100

    Clean up app.py by removing commented code
    Removed commented-out code for route registration.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from routes.informations import informations_bp
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# register routes
+
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(informations_bp, url_prefix="/informations")
 
