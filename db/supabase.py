08a4d4439ce0f3bdad113726fc835c89da2bb40a
billel khiri <145929851+bi2lelkhr@users.noreply.github.com>
2026-01-28 19:50:08 +0100

    Update supabase.py

--- db/supabase.py
+++ db/supabase.py
@@ -2,7 +2,7 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # 👈 loads .env automatically
+load_dotenv() 
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
